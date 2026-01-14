532988e9ae4cdaccc71a84958e8e4f0ee90edef1
didacfrndz <fernandezbarcodidac@fpllefia.com>
2026-01-14 20:44:15 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <section>
+        <div>
+        <h1>Aetherfield</h1>
+        <div>+</div>
+      </div>
+
+      <div>
+        <div>
+          <h2>Sustainability insights,</h2>
+          <h2>built for business</h2>
+          <p>Track impact, reduce emissions, and accelerate progress--with clarity and confidence.</p>
+        </div>
+        <div>
+          <button> * request a demo</button>
+          <button> * Explore the platform</button>
+        </div>
+      </div>
+      <img src="" alt="img1" />
+      </section>
+      
+      <section>
+        <div>
+          <h2>Everything you need to measure, model, and act on sustainability</h2>
+        </div>
+        <img src="" alt="" />
+        ------
+        <div>
+          <div>
+            <div>
+              <h3>Track</h3>
+              <p>001</p>
+            </div>
+            <p>Emissions, energy, and waste across your value chain</p>
+          </div>
+          -------
+          <div>
+            <div>
+              <h3>Model</h3>
+              <p>002</p>
+            </div>
+            <p>Forecast performance and goal alignment</p>
+          </div>
+          -------
+          <div>
+            <div>
+              <h3>Report</h3>
+              <p>003</p>
+            </div>
+            <p>Generate ESG disclosures, automate frameworks</p>
+          </div>
+          -------
+          <div>
+            <div>
+              <h3>Act</h3>
+              <p>004</p>
+            </div>
+            <p>Surface insights and operational next steps</p>
+          </div>
+          -------
+          <button>* Explore features</button>
+        </div>
+      </section>
+
+    </div>
+  )
+}
+
+export default App
